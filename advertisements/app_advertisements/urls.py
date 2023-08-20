b4edc2295986dbcdf1de4f1a2271a633c1effda9
Рафаэль <Khaliullin.rafael@bk.ru>
2023-08-20 13:33:28 +0800

    Отладка

--- advertisements/app_advertisements/urls.py
+++ advertisements/app_advertisements/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import index,top_sellers
+from .views import index, top_sellers, advertisement_post, register, login, profile, novelties
 
 urlpatterns = [
     path('', index, name='main-page'),
     path('top-sellers/',top_sellers, name='top-sellers'),
-    path('index/',index, name='index')
-
+    path('index/',index, name='index'),
+    path('advertisement-post/', advertisement_post, name='adv-post'),
+    path('novelties/', novelties, name='news'),
 ]
 
